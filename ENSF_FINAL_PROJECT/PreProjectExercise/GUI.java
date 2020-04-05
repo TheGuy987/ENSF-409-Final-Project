1f1383987a0fde3b2fd273e1d283b3820a7f0aa9
Thomas <thomastppt@gmail.com>
2020-04-05 16:13:07 -0600

    First push.
    Created GUI, currently working on adding listeners to the buttons.

---
+++ ENSF_FINAL_PROJECT/PreProjectExercise/GUI.java
@@ -0,0 +1,43 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class GUI extends JFrame {
+	JButton insert, find, browse, createTree;
+	JLabel title;
+	JPanel titlePanel, buttonPanel;
+	JScrollPane display;
+	
+	public GUI() {
+		titlePanel = new JPanel();
+		title = new JLabel("An Application to Maintain Student Records");
+		titlePanel.add(title);
+		
+		insert = new JButton("Insert");
+		find = new JButton("Find");
+		browse = new JButton("Browse");
+		createTree = new JButton("Create Tree From File");
+		
+		//insert.addActionListener();
+		
+		buttonPanel = new JPanel();
+		buttonPanel.add(insert);
+		buttonPanel.add(find);
+		buttonPanel.add(browse);
+		buttonPanel.add(createTree);
+		
+		display = new JScrollPane();
+		
+		this.add("South",buttonPanel);
+		this.add("North",titlePanel);
+		this.add("Center",display);
+		this.setSize(720,480);
+	}
+	
+	public static void main(String[] args) {
+		GUI app = new GUI();
+		app.setVisible(true);
+	}
+}
