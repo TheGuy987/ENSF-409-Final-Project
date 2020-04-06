73926a127e19c34183e7065ffdfb65a75f8549bf
Matt Wells <mswells@hotmail.ca>
2020-04-05 20:55:13 -0600

    Added javadoc to the Controller class

--- ENSF Pre-Project/src/Controller.java
+++ ENSF Pre-Project/src/Controller.java
@@ -14,15 +14,30 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.OverlayLayout;
 
+/**
+ * Class that takes handles the communication between a GUI object and a BinSearchTree object.
+ * It has various methods that corrospond to button presses on the GUI.
+ * @author Vaibhav Kapoor, Thomas Pan, and Matthew Wells
+ *
+ */
 public class Controller {
 	BinSearchTree bst;
 	GUI mainPane;
 	
+	/**
+	 * Constructor that takes GUI object as input, variable mainPane to it, and assigns variable
+	 * bst to a new BinSearchTree object.
+	 * @param mainPane GUI object that controller will communicate with.
+	 */
 	public Controller(GUI mainPane) {
 		bst = new BinSearchTree();
 		this.mainPane = mainPane;
 	}
 	
+	/**
+	 * Corrosponds to the "insert" button on the GUI. Prompts the user for Student ID, Faculty, Major,
+	 * and Year of Study. It then uses this for a new entry into the Binary Search Tree.
+	 */
 	public void insertPressed() {
 		JPanel myPanel = new JPanel();
 		
@@ -55,6 +70,12 @@ public class Controller {
 	}
 }
 	
+	/**
+	 * Corrosponds to the "find" button on the GUI. It prompts the user for a student's id, then 
+	 * searches the BinSearchTree object bst for a corrosponding Node with the same id. If found, it
+	 * displays the information about the student in a Jpane, otherwise conveys that there is no
+	 * matching Node in a Jpane.
+	 */
 	public void findPressed() {
 		// Tree exists
 		if(bst.root!=null) {
@@ -75,6 +96,10 @@ public class Controller {
 		}
 	}
 	
+	/**
+	 * Corrsponds to the "browse" button of the GUI. If variable bst is not empty, it displays the contents of
+	 * variable bst on the main GUI.
+	 */
 	public void browsePressed() {
 		mainPane.remove(mainPane.display);
 		if(bst.root != null)
@@ -87,6 +112,11 @@ public class Controller {
         mainPane.pack();
 	}
 	
+	/**
+	 * Uses a PrintWriter and a StringWriter to create a new JTextArea that displays the
+	 * contents of the BinSearchTree object bst.
+	 * @return JTextArea that contains the output of the BinSearchTree method print_tree.
+	 */
 	public JTextArea updateTextArea() {
 		StringWriter out = new StringWriter();
 		PrintWriter writer = new PrintWriter(out);
@@ -100,6 +130,13 @@ public class Controller {
 		return jta;
 	}
 
+	/**
+	 * Corrosponds to the "create tree from file" button on the GUI. It prompts the user
+	 * for a file name using a Jpane, and then uses the input to create a DataBaseReader
+	 * object. The DataBaseReader object then attempts to read from the specified file, and
+	 * fill variable bsd with its contents.
+	 */
+	
 	public void createTreePressed() {
 		String fileName = JOptionPane.showInputDialog("Enter the file name:");
 		if(fileName != null) {
