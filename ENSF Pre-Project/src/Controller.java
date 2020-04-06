638c23da10f2485895649a05921c4118625e798f
Matt Wells <mswells@hotmail.ca>
2020-04-05 18:25:35 -0600

    Stopped program from throwing an error whenever "cancel" is hit in the tree creation option.

--- ENSF Pre-Project/src/Controller.java
+++ ENSF Pre-Project/src/Controller.java
@@ -1,11 +1,8 @@
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 
 public class Controller {
-	private BinSearchTree bst;
+	BinSearchTree bst;
 	
 	public Controller() {
 		bst = new BinSearchTree();
@@ -22,20 +19,34 @@ public class Controller {
 	}
 	
 	public void findPressed() {
-		
+		// Tree exists
+		if(bst.root!=null) {
+			String idToSearch = JOptionPane.showInputDialog("Please enter the student's id:");
+			Node nodeFind = bst.find(bst.root, idToSearch);
+			
+			// Record exists
+			if(nodeFind != null) {
+				JOptionPane.showMessageDialog(null, "Id: " + nodeFind.data.id + "\nFaculty: " + nodeFind.data.faculty + "\nMajor: " + nodeFind.data.major + "\nYear: " + nodeFind.data.year, "Search Results", JOptionPane.INFORMATION_MESSAGE, null);
+			}
+			// Record doesn't exists
+			else {
+				JOptionPane.showMessageDialog(null, "Id: " + nodeFind.data.id + "\nFaculty: " + nodeFind.data.faculty + "\nMajor: " + nodeFind.data.major + "\nYear: " + nodeFind.data.year, "Search Results", JOptionPane.INFORMATION_MESSAGE, null);
+			}
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Please Create a Tree before using Insert","Error", JOptionPane.PLAIN_MESSAGE);
+		}
 	}
 	
 	public void browsePressed() {
-		if(bst.root==null) {
-			JOptionPane.showMessageDialog(null, "Please Create a Tree before using Browse","Error", JOptionPane.PLAIN_MESSAGE);
-		}else {
-			
-		}
+	
 	}
 
 	public void createTreePressed() {
 		String fileName = JOptionPane.showInputDialog("Enter the file name:");
-		DataBaseReader createTree = new DataBaseReader(fileName, bst);
-		createTree.readFromFile();
+		if(fileName != null) {
+			DataBaseReader createTree = new DataBaseReader(fileName, bst);
+			createTree.readFromFile();
+		}
 	}
 }
