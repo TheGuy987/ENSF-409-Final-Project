375620f4aaf41728ecef6f2259f38ce9025bd709
Matt Wells <mswells@hotmail.ca>
2020-04-05 20:04:05 -0600

    Added javadoc for DataBaseReader

--- ENSF Pre-Project/src/Controller.java
+++ ENSF Pre-Project/src/Controller.java
@@ -1,5 +1,8 @@
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Controller {
 	BinSearchTree bst;
@@ -9,12 +12,34 @@ public class Controller {
 	}
 	
 	public void insertPressed() {
-		if(bst.root==null) {
-			JOptionPane.showMessageDialog(null, "Please Create a Tree before using Insert","Error", JOptionPane.PLAIN_MESSAGE);
-		}else {
-			JDialog insertFields = new JDialog();
-			insertFields.setSize(500, 300);
-			insertFields.setVisible(true);
+		JPanel myPanel = new JPanel();
+			
+		JLabel idPrompt = new JLabel("Enter the Student ID");
+		JLabel facultyPrompt = new JLabel("Enter Faculty");
+		JLabel majorPrompt = new JLabel("Enter Student's Major");
+		JLabel yearPrompt = new JLabel("Enter year");
+			
+		JTextField idField = new JTextField(5);
+		JTextField facultyField = new JTextField(2);
+		JTextField majorField = new JTextField(4);
+		JTextField yearField = new JTextField(1);
+			
+		myPanel.add(idPrompt);
+		myPanel.add(idField);
+		
+		myPanel.add(facultyPrompt);
+		myPanel.add(facultyField);
+		
+		myPanel.add(majorPrompt);
+		myPanel.add(majorField);
+		
+		myPanel.add(yearPrompt);
+		myPanel.add(yearField);
+
+		int check = JOptionPane.showConfirmDialog(null, myPanel,"Insert a New Node", JOptionPane.OK_CANCEL_OPTION);
+		if(check==JOptionPane.OK_OPTION) {
+			bst.insert(idField.getText(), facultyField.getText(), majorField.getText(), yearField.getText());
+			JOptionPane.showMessageDialog(null, "Your Node has been added for student "+idField.getText(), "Insert", JOptionPane.PLAIN_MESSAGE);
 		}
 	}
 	
@@ -30,11 +55,11 @@ public class Controller {
 			}
 			// Record doesn't exists
 			else {
-				JOptionPane.showMessageDialog(null, "Id: " + nodeFind.data.id + "\nFaculty: " + nodeFind.data.faculty + "\nMajor: " + nodeFind.data.major + "\nYear: " + nodeFind.data.year, "Search Results", JOptionPane.INFORMATION_MESSAGE, null);
+				JOptionPane.showMessageDialog(null, "Error: Record not found!", "Error", JOptionPane.INFORMATION_MESSAGE, null);
 			}
 		}
 		else {
-			JOptionPane.showMessageDialog(null, "Please Create a Tree before using Insert","Error", JOptionPane.PLAIN_MESSAGE);
+			JOptionPane.showMessageDialog(null, "Please Create a Tree before using Insert", "Error", JOptionPane.PLAIN_MESSAGE);
 		}
 	}
 	
--- ENSF Pre-Project/src/DataBaseReader.java
+++ ENSF Pre-Project/src/DataBaseReader.java
@@ -6,17 +6,35 @@ import java.io.IOException;
 import javax.swing.JOptionPane;
 
 
-
+/**
+ * Class that uses a file to fill a binary Search Tree. It has variables that hold the
+ * name of the input file, and an initialized Binary Search Tree to be filled.
+ * @author Vaibhav Kapoor, Thomas Pan, and Matthew Wells
+ *
+ */
 public class DataBaseReader {
 	
 	private BinSearchTree theTree;
 	private String fileName;
 
+	/**
+	 * Constructor that takes in a file name and a BinSearchTree object. It sets these to
+	 * the corrosponding instance variables.
+	 * @param fileName String to set variable fileName to.
+	 * @param theTree BinSearchTree object that will be populated using the contents of the 
+	 * file specified by fileName.
+	 */
 	public DataBaseReader(String fileName, BinSearchTree theTree) {
 		this.fileName = fileName;
 		this.theTree = theTree;	
 	}
 	
+	/**
+	 * Creates a BefferedReader object to read from the specified file, then goes through the input
+	 * file line by line, calling the helper method addToTree for each line read. If the file
+	 * specified by variable fileName is not found, the method creates a Jpane to alert the user,
+	 * and ends early.
+	 */
 	public void readFromFile() {
 		BufferedReader input;
 		try {
@@ -47,6 +65,12 @@ public class DataBaseReader {
 		
 	}
 	
+	/**
+	 * Helper method called by readFromFile. It seperates a line read from the input
+	 * file into ID number, faculty, major, and year of study. Then it creates a new
+	 * Node for theTree object using this information.
+	 * @param line String holding the contents of one line of the input file.
+	 */
 	private void addToTree(String line) {
 		String[] separated = line.trim().split("\\s+");
 		theTree.insert(separated[0], separated[1], separated[2], separated[3]);
