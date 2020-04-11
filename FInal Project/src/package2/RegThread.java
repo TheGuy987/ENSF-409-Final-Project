7732871daed5eefffae65a9da28acd02eb83d9ff
Matt Wells <mswells@hotmail.ca>
2020-04-11 15:02:19 -0600

    Created classes RegServer and RegThread

---
+++ FInal Project/src/package2/RegThread.java
@@ -0,0 +1,38 @@
+package package2;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+public class RegThread implements Runnable{
+
+	private Student theStudent;
+	private CourseCatalogue theCatalogue;
+	private BufferedReader socketIn;
+	private PrintWriter socketOut;
+	
+	@Override
+	public void run() {
+		
+	}
+	
+	public String removeCourse(String Course) {
+		
+	}
+	
+	public String viewCatCourses() {
+		
+	}
+	
+	public String searchCatCourses() {
+		
+	}
+	
+	public String addCourse(String course) {
+		
+	}
+	
+	public String viewStudentCourses(String course) {
+		
+	}
+	
+}
