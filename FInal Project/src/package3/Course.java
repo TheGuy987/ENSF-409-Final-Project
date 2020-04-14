891d843a89cf7f78e23bf27c3282648986696d17
Thomas <thomastppt@gmail.com>
2020-04-13 22:26:29 -0600

    Javadoc complete.. javadoc creds to Matt Wells :)

--- FInal Project/src/package3/Course.java
+++ FInal Project/src/package3/Course.java
@@ -1,14 +1,25 @@
 package package3;
 
 import java.util.ArrayList;
-
+/**
+ * Class that represents a course. It holds variables relating to the name and number of the
+ * course, and two Array Lists for the pre-reqs of the course and the course offerings relating
+ * the course.
+ * @author Vaibhav Kapoor, Thomas Pan, and Matthew Wells
+ *
+ */
 public class Course {
 
 	private String courseName;
 	private int courseNum;
 	private ArrayList<Course> preReq;
 	private ArrayList<CourseOffering> offeringList;
-
+	/**
+	 * Constructor that takes in a course name and number, and assigns them to the corrospoding
+	 * variables. It also creates new Array Lists for variables preReq and offeringList.
+	 * @param courseName String holding the name of the course.
+	 * @param courseNum Integer holding the coures number.
+	 */
 	public Course(String courseName, int courseNum) {
 		this.setCourseName(courseName);
 		this.setCourseNum(courseNum);
@@ -16,7 +27,10 @@ public class Course {
 		preReq = new ArrayList<Course>();
 		offeringList = new ArrayList<CourseOffering>();
 	}
-
+	/**
+	 * Adds an offering to the course. Checks to make sure the offering is for this course.
+	 * @param offering Course offering to be added to the course.
+	 */
 	public void addOffering(CourseOffering offering) {
 		if (offering != null && offering.getTheCourse() == null) {
 			offering.setTheCourse(this);
@@ -29,40 +43,67 @@ public class Course {
 			offeringList.add(offering);
 		}
 	}
-
+	/**
+	 * Returns the course name.
+	 * @return String holding the name of the course.
+	 */
 	public String getCourseName() {
 		return courseName;
 	}
-
+	/**
+	 * Sets the course name.
+	 * @param courseName String holding the name to set courseName to.
+	 */
 	public void setCourseName(String courseName) {
 		this.courseName = courseName;
 	}
-
+	/**
+	 * Returns the course number.
+	 * @return Integer holding the course number.
+	 */
 	public int getCourseNum() {
 		return courseNum;
 	}
-
+	/**
+	 * Sets the course number.
+	 * @param courseNum Integer to set the course number to.
+	 */
 	public void setCourseNum(int courseNum) {
 		this.courseNum = courseNum;
 	}
-	
+	/**
+	 * Returns the list of pre reqs for the course.
+	 * @return Array List holding the list of pre reqs for the course.
+	 */
 	public ArrayList<Course> getPreReq() {
 		return preReq;
 	}
-	
+	/**
+	 * Returns the list of pre reqs for the course.
+	 * @return Array List holding the list of pre reqs for the course.
+	 */
 	public ArrayList<CourseOffering> getOfferingList(){
 		return offeringList;
 	}
-	
+	/**
+	 * adds a Pre Req course to variable preReq.
+	 * @param course object to be added to variable preReq.
+	 */
 	public void addPreReq(Course course) {
 		preReq.add(course);
 	}
-	
+	/**
+	 * removes a Pre Req course to variable preReq.
+	 * @param course object to be removed to variable preReq.
+	 */
 	public void removePreReq(Course course) {
 		preReq.remove(course);
 	}
 	
-	
+	/**
+	 * Returns the object as a string
+	 * @return String that represetns the object as a String.
+	 */
 	@Override
 	public String toString () {
 		String st = "\n";
@@ -73,7 +114,11 @@ public class Course {
 		st += "\n-------\n";
 		return st;
 	}
-
+	/**
+	 * Returns the course offering at a certain index of the ArrayList variable offeringList.
+	 * @param i Integer holding the index of variable offeringList.
+	 * @return CourseOffering at the specified index of variable offeringList.
+	 */
 	public CourseOffering getCourseOfferingAt(int i) {
 		// TODO Auto-generated method stub
 		if (i < 0 || i >= offeringList.size() )
--- FInal Project/src/package3/Registration.java
+++ FInal Project/src/package3/Registration.java
@@ -1,41 +1,78 @@
 package package3;
-
+/**
+ * Class representing a students registration in a specific course. It has variables for
+ * the student, the course offering, and the grade the student currently has in the course.
+ * @author Vaibhav Kapoor, Thomas Pan, and Matthew Wells
+ *
+ */
 public class Registration {
 	private Student theStudent;
 	private CourseOffering theOffering;
 	private char grade;
-	
+	/**
+	 * assigns the variables theStudent and theOffering, then calls the method addRegistration.
+	 * @param st Student object assigned to theStudent.
+	 * @param of CourseOffering object assigned to theOffering.
+	 */	
 	void completeRegistration (Student st, CourseOffering of) {
 		theStudent = st;
 		theOffering = of;
 		addRegistration ();
 	}
-	
+	/**
+	 * Adds this object to the Student object and the CourseOffering object.
+	 */
 	private void addRegistration () {
 		theStudent.addRegistration(this);
 		theOffering.addRegistration(this);
 	}
 	
-	
+	/**
+	 * Returns theStudent.
+	 * @return object of type Student to be returned.
+	 */
 	public Student getTheStudent() {
 		return theStudent;
 	}
+	/**
+	 * Sets theStudent
+	 * @param theStudent object of type Student to set theStudent to.
+	 */
 	public void setTheStudent(Student theStudent) {
 		this.theStudent = theStudent;
 	}
+	/**
+	 * Returns theOffering
+	 * @return object of type CourseOffering to be returned.
+	 */
 	public CourseOffering getTheOffering() {
 		return theOffering;
 	}
+	/**
+	 * Sets theOffering
+	 * @param theOffering object of type CourseOffering to set theOffering to.
+	 */
 	public void setTheOffering(CourseOffering theOffering) {
 		this.theOffering = theOffering;
 	}
+	/**
+	 * Returns grade.
+	 * @return Char holding the value of grade.
+	 */
 	public char getGrade() {
 		return grade;
 	}
+	/**
+	 * Sets grade.
+	 * @param grade Char to set grade to.
+	 */
 	public void setGrade(char grade) {
 		this.grade = grade;
 	}
-	
+	/**
+	 * Returns this class in String form.
+	 * @return String holding this class in String form.
+	 */
 	@Override
 	public String toString () {
 		String st = "\n";
