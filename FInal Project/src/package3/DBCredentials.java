0cf01ec4ab2c5b631def11a004178af711ee4e20
Matt Wells <mswells@hotmail.ca>
2020-04-18 21:34:09 -0600

    plz

---
+++ FInal Project/src/package3/DBCredentials.java
@@ -0,0 +1,12 @@
+package package3;
+
+public interface DBCredentials {
+	
+
+	static final String USER = "root";
+	static final String PASS = "ENSF409W20Final_Project";
+	static final String URL = "jdbc:mysql://localhost:3306";
+	static final String DRIVER = "theComm.mysql.cj.jdbc.Driver";
+	
+	
+}
